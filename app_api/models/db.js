13c12b980b49a8eec52c4821e8cc5639509c38a7
mduzac <mduzac@mymail.mines.edu>
2018-04-17 15:50:33 -0600

    Added rest of subfolders to app_api

---
+++ app_api/models/db.js
@@ -0,0 +1,46 @@
+var mongoose = require( 'mongoose' );
+var gracefulShutdown;
+var dbURI = 'TODO';
+if(process.env.NODE_ENV==='production'){
+  dbURI = 'TODO';
+}
+mongoose.connect(dbURI);
+
+var gracefulShutdown = function(msg, callback) {
+  mongoose.connection.close(function() {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+
+process.once ('SIGUSR2', function() {
+  gracefulShutdown('nodemon restart', function() {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+
+process.on ('SIGINT', function() {
+  gracefulShutdown('app termination', function() {
+    process.exit(0);
+  });
+});
+
+process.on ('SIGTERM', function() {
+  gracefulShutdown('Heroku app shutdown', function() {
+    process.exit(0);
+  });
+});
+
+mongoose.connection.on('connected', function() {
+  console.log('Mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', function(err) {
+  console.log('Mongoose connected error ' + err);
+});
+
+mongoose.connection.on('disconnected', function() {
+  console.log('Mongoose disconnected');
+});
+
+require('.listings');
