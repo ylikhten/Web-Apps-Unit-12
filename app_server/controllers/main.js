5229e51526d4889f8f1e469046aa1b842c243b88
mduzac <mduzac@mymail.mines.edu>
2018-04-18 09:02:49 -0600

    About page, Single Listing Page, and Add Listing page files added.  About page filled in with text

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,4 +1,19 @@
 /* GET home page */
 module.exports.index = function(req, res) {
-    res.render('index', {title: 'BookBarterCSM'});
+    res.render('allListings', {title: 'BookBarterCSM'});
+};
+
+/*GET about page */
+module.exports.about = function(req, res) {
+    res.render('about', {title: 'About'});
+};
+
+/*GET Single Listing */
+module.exports.singleListing = function(req, res) {
+    res.render('singleListing', {title: 'Item'});
+};
+
+/*GET Add Listing Form */
+module.exports.newListingForm = function(req, res) {
+    res.render('addListingForm', {title: 'New Listing'});
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,5 +4,8 @@ var ctrlMain = require('../controllers/main');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
+router.get('/about', ctrlMain.about);
+router.get('/listings/add', ctrlMain.newListingForm);
+router.get('listings/item', ctrlMain.singleListing);
 
 module.exports = router;
