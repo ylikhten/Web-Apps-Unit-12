9216a238cdb0d2dda9b53c3b62b1ab267acdab82
mduzac <mduzac@mymail.mines.edu>
2018-04-18 09:24:12 -0600

    Finished add listing and single listing pages (very basic).  Added a header and footer to all pages.  Added some very basic css

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -6,6 +6,6 @@ var ctrlMain = require('../controllers/main');
 router.get('/', ctrlMain.index);
 router.get('/about', ctrlMain.about);
 router.get('/listings/add', ctrlMain.newListingForm);
-router.get('listings/item', ctrlMain.singleListing);
+router.get('/listings/item', ctrlMain.singleListing);
 
 module.exports = router;
